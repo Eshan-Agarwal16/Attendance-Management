3b7fbb83c18617f577aedb7cbf126ec88b6ade51
Eshan-Agarwal16 <61204624+Eshan-Agarwal16@users.noreply.github.com>
2021-06-16 13:09:46 +0400

    student update

--- student.py
+++ student.py
@@ -4,14 +4,24 @@ from PIL import Image, ImageTk
 
 class student:
   def __init__(self,root):
+
     self.root = root
     self.root.title("Attendance Manager")
     self.root.state('zoomed')
-    self.root['background'] = "#cbf3f0"
+    # self.root.configure(bg="#cbf3f0")
 
+    screen_width = self.root.winfo_screenwidth()
+    screen_height = self.root.winfo_screenheight()
 
+    background_frame = Frame(self.root,bg = "#cbf3f0")
+    background_frame.place(x=0,y=0,width=screen_width,height=screen_height)
 
+    title_size = 70*screen_width
+    title_size = int(title_size/1920)
+    heading = Label(background_frame ,text = "STUDENT DETAILS",font = ("Berlin Sans FB",title_size),fg= "#2ec4b6",bg = "#cbf3f0")
+    heading.place(x=0,y=20,width=screen_width)
 
+    
 
 if __name__=="__main__":
   root=Tk()
